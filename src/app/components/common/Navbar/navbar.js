7a1c27ae204d741fc34fe9aab91169ecd1adaeac
Siddhartha Sarkar <siddsarkar7@gmail.com>
2021-04-12 20:24:14 +0530

    use svg components

--- src/app/components/common/Navbar/navbar.js
+++ src/app/components/common/Navbar/navbar.js
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom'
 import logo from '../../../assets/rr_logo.svg'
+import { Menu, Search } from '../../icons'
 import { Button } from '../../ui'
 import './navbar.scss'
 
@@ -8,17 +9,7 @@ function NavBar({ opensearch }) {
         <div className="navbar">
             <div className="navbar-actions">
                 <Button className="btn-rectangle">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 16 16"
-                        width="16"
-                        height="16"
-                    >
-                        <path
-                            fillRule="evenodd"
-                            d="M1 2.75A.75.75 0 011.75 2h12.5a.75.75 0 110 1.5H1.75A.75.75 0 011 2.75zm0 5A.75.75 0 011.75 7h12.5a.75.75 0 110 1.5H1.75A.75.75 0 011 7.75zM1.75 12a.75.75 0 100 1.5h12.5a.75.75 0 100-1.5H1.75z"
-                        ></path>
-                    </svg>
+                    <Menu />
                 </Button>
                 <Link to="/">
                     <img
@@ -32,17 +23,7 @@ function NavBar({ opensearch }) {
                 </Link>
 
                 <Button onClick={opensearch} className="btn-circle">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        width="24"
-                        height="24"
-                    >
-                        <path
-                            fillRule="evenodd"
-                            d="M14.53 15.59a8.25 8.25 0 111.06-1.06l5.69 5.69a.75.75 0 11-1.06 1.06l-5.69-5.69zM2.5 9.25a6.75 6.75 0 1111.74 4.547.746.746 0 00-.443.442A6.75 6.75 0 012.5 9.25z"
-                        ></path>
-                    </svg>
+                    <Search />
                 </Button>
             </div>
         </div>
--- src/app/components/ui/FloatingButton/floatingbutton.js
+++ src/app/components/ui/FloatingButton/floatingbutton.js
@@ -1,5 +1,6 @@
 import { useRef } from 'react'
 import { useHistory } from 'react-router-dom'
+import { Menu, Swap } from '../../icons'
 import './floatingbutton.scss'
 
 const FloatingButton = ({ props }) => {
@@ -12,6 +13,11 @@ const FloatingButton = ({ props }) => {
 
     const toggleNavExpand = () => {
         // cBottomNavRef.current.classList.toggle('unhide')
+        if (
+            userFloatRef.current.classList.contains('user-popup-open')
+        ) {
+            toggleUserExpand()
+        }
         floatRef.current.classList.toggle('float-hidden')
         bottomNavRef.current.classList.toggle('bottom-navbar-open')
     }
@@ -27,18 +33,7 @@ const FloatingButton = ({ props }) => {
                 onClick={toggleNavExpand}
                 className="floating-action-btn"
             >
-                <svg
-                    className="floating-icon"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 16 16"
-                    width="16"
-                    height="16"
-                >
-                    <path
-                        fillRule="evenodd"
-                        d="M1 2.75A.75.75 0 011.75 2h12.5a.75.75 0 110 1.5H1.75A.75.75 0 011 2.75zm0 5A.75.75 0 011.75 7h12.5a.75.75 0 110 1.5H1.75A.75.75 0 011 7.75zM1.75 12a.75.75 0 100 1.5h12.5a.75.75 0 100-1.5H1.75z"
-                    ></path>
-                </svg>
+                <Menu />
             </div>
             {/* to block */}
             {/* <div
@@ -50,14 +45,7 @@ const FloatingButton = ({ props }) => {
                     onClick={toggleNavExpand}
                     className="bottom-navbar-hide-btn"
                 >
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        width="24"
-                        height="24"
-                    >
-                        <path d="M7.72 21.78a.75.75 0 001.06-1.06L5.56 17.5h14.69a.75.75 0 000-1.5H5.56l3.22-3.22a.75.75 0 10-1.06-1.06l-4.5 4.5a.75.75 0 000 1.06l4.5 4.5zm8.56-9.5a.75.75 0 11-1.06-1.06L18.44 8H3.75a.75.75 0 010-1.5h14.69l-3.22-3.22a.75.75 0 011.06-1.06l4.5 4.5a.75.75 0 010 1.06l-4.5 4.5z"></path>
-                    </svg>
+                    <Swap />
                 </div>
                 {/* <div className="bottom-navbar-title">MENU</div> */}
                 <div className="bottom-navbar-menuitems">
@@ -67,7 +55,12 @@ const FloatingButton = ({ props }) => {
                     >
                         Home
                     </div>
-                    <div className="bottom-navbar-menuitems-item">
+                    <div
+                        onClick={() =>
+                            history.push('/user/bookmarks')
+                        }
+                        className="bottom-navbar-menuitems-item"
+                    >
                         Archives
                     </div>
                     <div className="bottom-navbar-menuitems-item">
